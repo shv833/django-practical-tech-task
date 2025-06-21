c76079da1d4db2c4de44fa46f5984a387325ae89
Volodymyr Shyra <shyra.volodymyr.833@gmail.com>
2025-06-22 00:26:21 +0300

    Fix Bad requests deploy

--- CVProject/entrypoint.sh
+++ CVProject/entrypoint.sh
@@ -11,6 +11,6 @@ else
     python manage.py migrate
     python manage.py collectstatic --noinput
     python manage.py loaddata main/fixtures/cv_fixture.json
-    uvicorn CVProject.asgi:application --host 0.0.0.0 --port 8000
+    uvicorn CVProject.asgi:application --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips "*"
 fi
 exec "$@"
